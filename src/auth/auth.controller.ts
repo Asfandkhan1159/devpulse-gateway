ed495a530bd2c180a13922281829792f8004096e
AsfandKhan1159 <asfand.khan5024@gmail.com>
2026-05-29 20:50:43 +0500

    allowed cross origin for a moment

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -20,7 +20,7 @@ export class AuthController {
     response.cookie('access_token', access_token,{
       httpOnly:true,
       secure:process.env.NODE_ENV === 'production',
-      sameSite:'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
       maxAge:24 * 60 * 60 * 1000, 
 
     })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
   response.cookie('access_token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     maxAge: 24 * 60 * 60 * 1000,
   });
   return token;
